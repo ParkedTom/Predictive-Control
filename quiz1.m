25d4051f9734f04e78ba366d5480ded7aaf0ec45
Tom Parker <tom.ed.parker@gmail.com>
2013-10-10 14:08:56 +0100

    initial commit

---
+++ quiz1.m
@@ -0,0 +1,3 @@
+Ac = [-1.2822,0,0.98,0;0,0,1,0;-5.4293,0,-1.8366,0;-128.2,128.2,0,0]; %continuous  time state free response matrix
+Bc = [-0.3;0;-17;0] %continuous time forced response matrix
+C = [0,1,0,0;0,0,0,1;-128.2,128.2,0,0] % state-output matrix
