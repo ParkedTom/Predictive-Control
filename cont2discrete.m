ec8a8bff4382922d4a2af0a9f83aecfb281c23b0
Tom Parker <tom.ed.parker@gmail.com>
2013-10-10 15:46:26 +0100

    added function to convert from continuous time to discrete time

---
+++ cont2discrete.m
@@ -0,0 +1,10 @@
+function [ A, B ] = cont2discrete( F, G, Ts )
+%cont2discrete creates the discrete time version of the continous state
+%space model, F and G with sampling time Ts
+
+A = expm(F*Ts); % calculate Discrete time free response
+B = G*expm(F*Ts)*G; % calculate discrete time forced response
+
+
+end
+
